b8bd1644c9fad9f5c4b9c029490dbe5487f99c14
Oleh Hryhorov <golantster@gmail.com>
2018-06-22 13:16:04 +0300

    update

--- deploy-virtual-edge-mom.groovy
+++ deploy-virtual-edge-mom.groovy
@@ -17,6 +17,7 @@
  */
 
 common = new com.mirantis.mk.Common()
+salt = new com.mirantis.mk.Salt()
 
 def slave_node = 'python'
 
@@ -31,10 +32,40 @@ if (common.validInputParam('MOM_JOB')) {
 
 def salt_overrides_list = SALT_OVERRIDES.tokenize('\n')
 
+def enableSyndic(saltMasterURL, nodeName, saltMasterCred, saltMoMIP) {
+
+    def saltMaster
+    saltMaster = salt.connection(saltMasterURL, saltMasterCred)
+
+    // Set up test_target parameter on node level
+    def fullnodename = salt.getMinions(saltMaster, nodeName).get(0)
+    def saltMasterExpression = 'I@salt:master'
+    def saltMasterTarget = ['expression': saltMasterExpression, 'type': 'compound']
+    def result
+    def conf_public_net = false
+
+
+    def classes_to_add = ['system.salt.syndic.single']
+    def params_to_add = ['salt_syndic_master_address': saltMoMIP]
+
+    result = salt.runSaltCommand(saltMaster, 'local', saltMasterTarget, 'reclass.node_update', null, null, ['name': "${fullnodename}", 'classes': classes_to_add, 'parameters': params_to_add])
+    salt.checkResult(result)
+
+    common.infoMsg('Perform full refresh for all nodes')
+    salt.fullRefresh(saltMaster, '*')
+
+    if (salt.testTarget(saltMaster, 'I@salt:syndic:enabled:True')) {
+        salt.enforceState(saltMaster, 'I@salt:syndic:enabled', 'salt.syndic', true, true, null, false, 180, 2)
+    }
+
+
+}
+
 node(slave_node) {
 
     def momBuild
     def salt_mom_url
+    def salt_mom_ip
     def deploy_edges_infra = [:]
     def deploy_edges = [:]
     def edgeBuildsInfra = [:]
@@ -60,14 +91,17 @@ node(slave_node) {
         }
     }
 */
-    def EDGE_DEPLOY_SCHEMAS = '{os_ha_ovs: {deploy_job_name: "deploy-heat-os_ha_ovs", properties: {SLAVE_NODE: "python", STACK_INSTALL: "openstack,ovs", STACK_TEMPLATE: "os_ha_ovs", STACK_TYPE: "heat", FORMULA_PKG_REVISION: "testing", STACK_DELETE: false, STACK_CLUSTER_NAME: "os-ha-ovs-syndic"}}, k8s_ha_calico: {deploy_job_name: "deploy-heat-k8s_ha_calico", properties: {SLAVE_NODE: "python", STACK_INSTALL: "k8s,calico", STACK_TEMPLATE: "k8s_ha_calico", STACK_TYPE: "heat", FORMULA_PKG_REVISION: "testing", STACK_DELETE: false, STACK_CLUSTER_NAME: "k8s-ha-calico-syndic"}} }'
+//    def EDGE_DEPLOY_SCHEMAS = '{k8s_ha_calico: {deploy_job_name: "deploy-heat-k8s_ha_calico", properties: {SLAVE_NODE: "python", STACK_INSTALL: "k8s,calico", STACK_TEMPLATE: "k8s_ha_calico", STACK_TYPE: "heat", FORMULA_PKG_REVISION: "testing", STACK_DELETE: false, EXT: "and *ohryhorov-deploy-heat-k8s-ha-calico-59*", STACK_CLUSTER_NAME: "k8s-ha-calico"}} }'
+
+    def EDGE_DEPLOY_SCHEMAS = '{os_ha_ovs: {deploy_job_name: "deploy-heat-os_ha_ovs", properties: {SLAVE_NODE: "python", STACK_INSTALL: "openstack,ovs", STACK_TEMPLATE: "os_ha_ovs", STACK_TYPE: "heat", FORMULA_PKG_REVISION: "testing", STACK_DELETE: false, STACK_CLUSTER_NAME: "os-ha-ovs"}}, k8s_ha_calico: {deploy_job_name: "deploy-heat-k8s_ha_calico", properties: {SLAVE_NODE: "python", STACK_INSTALL: "k8s,calico", STACK_TEMPLATE: "k8s_ha_calico", STACK_TYPE: "heat", FORMULA_PKG_REVISION: "testing", STACK_DELETE: false, STACK_CLUSTER_NAME: "k8s-ha-calico"}} }'
+
 //    def EDGE_DEPLOY_SCHEMAS = '{os_ha_ovs: {deploy_job_name: "deploy-heat-os_ha_ovs", properties: {SALT_MASTER_URL: "http://172.17.49.56:6969", SLAVE_NODE: "python", STACK_INSTALL: "openstack,ovs", STACK_TEMPLATE: "os_ha_ovs", STACK_TYPE: "heat", FORMULA_PKG_REVISION: "testing", STACK_DELETE: false, STACK_CLUSTER_NAME: "os-ha-ovs-syndic"}}, k8s_ha_calico: {deploy_job_name: "deploy-heat-k8s_ha_calico", properties: {SALT_MASTER_URL: "http://172.17.49.52:6969", SLAVE_NODE: "python", STACK_INSTALL: "k8s,calico", STACK_TEMPLATE: "k8s_ha_calico", STACK_TYPE: "heat", FORMULA_PKG_REVISION: "testing", STACK_DELETE: false, STACK_CLUSTER_NAME: "k8s-ha-calico-syndic"}} }'
 
     def edge_deploy_schemas = readJSON text: EDGE_DEPLOY_SCHEMAS
 
 //    try {
-        stage('Deploy MoM stack'){
-            momBuild = build job: deployMoMJob, propagate: false, parameters: [
+/*        stage('Deploy MoM stack'){
+            momBuild = build job: deployMoMJob, propagate: true, parameters: [
                 [$class: 'StringParameterValue', name: 'FORMULA_PKG_REVISION', value: 'testing'],
                 [$class: 'StringParameterValue', name: 'STACK_CLUSTER_NAME', value: 'virtual-mcp11-aio'],
                 [$class: 'StringParameterValue', name: 'STACK_INSTALL', value: 'core'],
@@ -87,6 +121,7 @@ node(slave_node) {
             if (momBuild.result == 'SUCCESS') {
                 // get salt master url
                 salt_mom_url = "http://${momBuild.description.tokenize(' ')[1]}:6969"
+                salt_mom_ip = "${momBuild.description.tokenize(' ')[1]}"
                 node_name = "${momBuild.description.tokenize(' ')[2]}"
                 salt_overrides_list.add("salt_syndic_master_address: ${momBuild.description.tokenize(' ')[1]}")
                 common.infoMsg("Salt API is accessible via ${salt_mom_url}")
@@ -97,8 +132,11 @@ node(slave_node) {
 
             }
 
-        } 
+        } */
         stage('Deploy edge clouds'){
+            salt_overrides_list.add("salt_syndic_enabled: true")
+            salt_overrides_list.add("salt_syndic_master_address: 172.17.48.208")
+
             for (edge_deploy_schema in edge_deploy_schemas.keySet()) {
                 def props
                 def deploy_job
@@ -127,7 +165,7 @@ node(slave_node) {
                             [$class: 'StringParameterValue', name: 'HEAT_STACK_ZONE', value: HEAT_STACK_ZONE],
                             [$class: 'StringParameterValue', name: 'OPENSTACK_API_PROJECT', value: OPENSTACK_API_PROJECT],
                             [$class: 'StringParameterValue', name: 'SLAVE_NODE', value: props['SLAVE_NODE']],
-                            [$class: 'StringParameterValue', name: 'STACK_INSTALL', value: 'mom'],
+                            [$class: 'StringParameterValue', name: 'STACK_INSTALL', value: 'core'],
                             [$class: 'StringParameterValue', name: 'STACK_NAME', value: stack_name],
                             [$class: 'StringParameterValue', name: 'STACK_TEMPLATE', value: props['STACK_TEMPLATE']],
                             [$class: 'StringParameterValue', name: 'STACK_TEMPLATE_URL', value: 'https://github.com/ohryhorov/salt-syndic-master'],
@@ -153,21 +191,21 @@ node(slave_node) {
                 def props_
                 def current_salt_ip
                 def extra_target
+                def saltMasterURL
 
                 if (edgeBuildsInfra[ed_].result == 'SUCCESS') {
 //                    extra_target = "and *jopa"
-                    extra_target = "and *${edgeBuildsInfra[ed_].description.tokenize(' ')[0]}"
+                    extra_target = "and *${edgeBuildsInfra[ed_].description.tokenize(' ')[0]}*"
 //                    current_salt_ip = edgeBuildsInfra[ed_].description.tokenize(' ')[1]
+                    saltMasterURL = "http://${edgeBuildsInfra[ed_].description.tokenize(' ')[1]}:6969"
+
+                    salt_mom_ip = "172.17.48.208"
+                    salt_mom_url = "http://172.17.48.208:6969"
+
+                    enableSyndic(saltMasterURL, 'cfg01*', SALT_MASTER_CREDENTIALS, salt_mom_ip)
 
                     props_ = edge_deploy_schemas[ed_]['properties']
                     deploy_job = edge_deploy_schemas[ed_]['deploy_job_name']
-
-
-//
-// XXX
-//
-//                    salt_mom_url = "http://172.17.53.1:6969"
-
 
 
                     deploy_edges["Deploy ${ed_} with MoM"] = {
